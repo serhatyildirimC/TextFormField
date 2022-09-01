82a99c24857079ba9d6fe5fd953a308af568dd11
serhatyildirimC <yilldirimserhat@gmail.com>
2022-09-01 10:35:54 +0300

    validators_eklendi_degerler_kaydedildi

--- lib/text_form_field.dart
+++ lib/text_form_field.dart
@@ -9,6 +9,8 @@ class MyTextFormFiled extends StatefulWidget {
 }
 
 class _MyTextFormFiledState extends State<MyTextFormFiled> {
+  String _email = '', _password = '', _userName = '';
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +21,8 @@ class _MyTextFormFiledState extends State<MyTextFormFiled> {
         child: Padding(
           padding: const EdgeInsets.all(16),
           child: Form(
-            autovalidateMode: AutovalidateMode.always,
+            key: _formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
             child: Column(children: [
               TextFormField(
                 initialValue: 'Serhat',
@@ -27,6 +30,9 @@ class _MyTextFormFiledState extends State<MyTextFormFiled> {
                     border: OutlineInputBorder(),
                     hintText: 'UserName',
                     labelText: 'Username'),
+                onSaved: (deger) {
+                  _userName = deger!;
+                },
                 validator: (deger) {
                   if (deger!.length < 4) {
                     return 'Karakter Yetersiz';
@@ -45,6 +51,9 @@ class _MyTextFormFiledState extends State<MyTextFormFiled> {
                     border: OutlineInputBorder(),
                     hintText: 'e-mail',
                     labelText: 'e-mail'),
+                onSaved: (deger) {
+                  _email = deger!;
+                },
                 validator: (deger) {
                   if (!EmailValidator.validate(deger!)) {
                     return 'e-mail gecersiz';
@@ -57,11 +66,14 @@ class _MyTextFormFiledState extends State<MyTextFormFiled> {
                 height: 15,
               ),
               TextFormField(
-                initialValue: '',
+                initialValue: 'aaaaaa',
                 decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: 'Password',
                     labelText: 'Password'),
+                onSaved: (deger) {
+                  _password = deger!;
+                },
                 validator: (deger) {
                   if (deger!.length < 6) {
                     return 'sifre 6 karakterden uzun olmalı';
@@ -70,6 +82,29 @@ class _MyTextFormFiledState extends State<MyTextFormFiled> {
                   }
                 },
               ),
+              const SizedBox(
+                height: 15,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    bool valid = _formKey.currentState!.validate();
+                    if (valid) {
+                      _formKey.currentState!.save();
+                      String result =
+                          'UserName: $_userName\ne-Mail:$_email\nPassword:$_password';
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          backgroundColor: Colors.pink.shade200,
+                          content: Text(
+                            result,
+                            style: const TextStyle(fontSize: 24),
+                          ),
+                        ),
+                      );
+                      _formKey.currentState!.reset();
+                    }
+                  },
+                  child: const Text('kaydet'))
             ]),
           ),
         ),
